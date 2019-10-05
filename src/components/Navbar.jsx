a080e8428241e9fef227172e7db7c79c25b37d85
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-10-04 20:11:06 -0400

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDarkMode } from '../hooks/useDarkMode';
 import { useLocalStorage } from '../hooks/useLocalStorage';
+import { NavLink, Route } from 'react-router-dom';
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useDarkMode();
@@ -11,7 +12,11 @@ const Navbar = () => {
   return (
     <>
     <nav className="navbar">
-      <h1>Crypto Tracker</h1>      
+      <h1>Crypto Tracker</h1>     
+      <div className='links'>
+        <NavLink>Home</NavLink>  
+        <NavLink>Coins</NavLink>  
+      </div> 
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
